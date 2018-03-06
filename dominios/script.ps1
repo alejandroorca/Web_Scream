90d16b2e769d7f774f6742ac4f3ec31a94f843c7
alejandroorca <alejandro96esp@gmail.com>
2018-03-06 09:47:20 +0100

    new_files

---
+++ dominios/script.ps1
@@ -0,0 +1,11 @@
+# En la primera línea se conecta al dominio. Esta línea requiere ser modificada.
+$Path= "LDAP://CN=Users,DC=alorca,DC=1a"
+$name = "Alejandro OC"
+$NTname = "AlejandroOC"
+$ou = New-Object Directoryservices.DirectoryEntry($Path)
+$user = $ou.PSBase.Children.Add("CN=" + $name,'user')
+$user.PSBase.CommitChanges()
+$user.SAMAccountName = $NTname
+$user.Description = "User_1"
+$user.PSBase.CommitChanges()
+$user.SetPassword("root")
