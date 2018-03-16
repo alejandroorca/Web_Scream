beb425c4190116a6aed209b75cc5f207a0e76408
alejandroorca <alejandro96esp@gmail.com>
2018-03-16 14:22:24 +0100

    fix

--- ejercicios_pc/librerias/02xml.php
+++ ejercicios_pc/librerias/02xml.php
@@ -5,8 +5,10 @@ $xml = simplexml_load_file('course_catalog.xml');
 
 $department = $xml->addChild('department');
 $department->addAttribute('code', 'ISO');
-$department->addChild('title', 'Implantación de sistemas operativos');
-$department->addChild('chair');
+$department->addChild('title', 'Implantacion de sistemas operativos');
+$chair = $department->addChild('chair');
+$chair->
+
 $department->addChild('jefe');
 $department->addChild('first name', 'Alejandro');
 $department->addChild('last_name', 'OC');
@@ -15,7 +17,7 @@ $department->addAttribute('number', 'ISO123');
 $department->addAttribute('enrollment', '777');
 $department->addChild('professor');
 $department->addChild('first_name', 'Manuel Ignacio');
-$department->addChild('last_name', 'López Quintero');
+$department->addChild('last_name', 'Lopez Quintero');
 $department->addChild('course');
 $department->addAttribute('number', 'PAR123');
 $department->addAttribute( 'enrollment', '888');
@@ -23,10 +25,10 @@ $department->addChild('professor');
 $department->addChild('first_name', 'Sergio');
 $department->addChild('last_name', 'Padilla');
 $department->addChild('course');
-$department->addAttribute('number', 'SGBD');
+$department->addAttribute('number', 'SGBD123');
 $department->addAttribute('enrollment', '999');
 $department->addChild('professor');
-$department->addChild('first_name', 'José Manuel');
+$department->addChild('first_name', 'Jose Manuel');
 $department->addChild('last_name', 'Lizana');
 
 
