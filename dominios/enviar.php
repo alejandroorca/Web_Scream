73cc9c582584e346b5b11241743737fe8c962d55
alejandroorca <alejandro96esp@gmail.com>
2018-03-21 18:51:51 +0100

    'fix'

---
+++ dominios/enviar.php
@@ -0,0 +1,22 @@
+<?php 
+
+$mail='alejandro96esp@gmail.com'; 
+
+
+$nombre = $_POST['Nombre']; 
+$telefono = $_POST['Teléfono']; 
+$email = $_POST['Email']; 
+$mensaje = $_POST['Mensaje']; 
+
+$thank="index.html"; 
+
+$message = " 
+nombre:".$nombre." 
+telefono:".$telefono." 
+email:".$email." 
+mensaje:".$mensaje.""; 
+
+if (mail($mail,"Formulario de Consulta",$message)) 
+Header ("Location: $thank"; 
+
+?> 
