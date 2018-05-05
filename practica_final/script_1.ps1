46e9a18f2f7fcda3fba3b1a5d4e3a033aef53e08
alejandroorca <alejandro96esp@gmail.com>
2018-05-05 18:25:02 +0200

    new

--- practica_final/script_1.ps1
+++
@@ -1,4 +0,0 @@
-$Domain = 'alorca.1a'
-$hostname = Read-Host -Prompt 'Nombre del equipo'
-Rename-Computer $hostname
-Add-computer -domainname $Domain -Credential alorca\user -newname $hostname -Restart
